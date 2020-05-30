9781ef50e97205c72d88204359ae35305a5fdb42
SohyeonP <hidhen9714@naver.com>
2020-05-31 01:46:50 +0900

    change

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.set('view engine','ejs');
 
-app.get('/', (req, res) => res.send('Moing'))
+app.get('./html/', function(req,res){
+    res.render('main');
+})
 
 app.listen(port, () => console.log(`Example app listing on port ${port}`))
