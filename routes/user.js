bdb123fb512335b1cc91cc7f0a4036356f6aaafb
SohyeonP <hidhen9714@naver.com>
2020-06-13 17:24:48 +0900

    비밀번호 유효성 검사

--- routes/user.js
+++ routes/user.js
@@ -20,23 +20,37 @@ router.get("/sign_up", function (req, res, next) {
 
 })
 router.post("/sign_up", function (req, res) {
-    var m_user = {
-        user_email: req.body.email,
-        user_name: req.body.name,
-        user_pw: req.body.password
-    };
-    console.log(m_user);
+    var m_info = req.body;
 
-    user.create(m_user)
-        .then((result) => {
-            console.log("들어간 데이터:", result);
-            return res.redirect("/user/sign_in");
-        }).catch((err) => {
-            console.error(err);
-            next(err);
-        })
+    if(!m_info.email || !m_info.name || !m_info.password || !m_info.Confpassword){
+        res.send();
+    }else{
+        if(m_info.password == m_info.Confpassword){
+                   
+                    var m_user = {
+                        user_email: m_info.email,
+                        user_name: m_info.name,
+                        user_pw: m_info.password
+                    };
 
-})
+                    user.create(m_user)
+                    .then((result) => {
+                        console.log("들어간 데이터:", result);
+                        res.send({"Success":"로그인 가능"});
+                        return res.redirect("/user/sign_in");
+                    }).catch((err) => {
+                        console.error(err);
+                        next(err);
+                    })
+                   
+                
+            
+        }else{
+            res.send({"Worng":"비밀번호 안맞음"});
+        }
+    }
+
+});
 // router.post(("/sign_up", (req, res) => {
 //     console.log(req.body);
 
@@ -59,7 +73,7 @@ router.post("/sign_up", function (req, res) {
 router.get("/sign_in", function (req, res) {
     console.log("로그인 페이지 요청");
     res.render("login", {});
-})
+});
 
 // router.get("err",function(req,res){
 //     res.render(err);
